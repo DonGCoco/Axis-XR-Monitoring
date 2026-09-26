abc6b4eeee00708b3fb45bf52cbd7a9510066f64
DONG Jieru <91780000+DonGCoco@users.noreply.github.com>
2026-09-26 05:14:55 +0200

    Add pinch press feedback to XR controls

--- Assets/Scripts/XRClickable.cs
+++ Assets/Scripts/XRClickable.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,11 @@ public class XRClickable : MonoBehaviour
     private Action _onClick;
     private Color _normalColor;
     private Color _hoverColor;
+    private Color _pressedColor;
     private bool _hovered;
+    private bool _pressed;
+    private Vector3 _normalScale;
+    private Coroutine _pressRoutine;
 
     public void Initialize(
         Image targetImage,
@@ -20,6 +25,8 @@ public class XRClickable : MonoBehaviour
         _onClick = onClick;
         _normalColor = normalColor;
         _hoverColor = hoverColor;
+        _pressedColor = Color.Lerp(hoverColor, Color.white, 0.18f);
+        _normalScale = transform.localScale;
         ApplyColor();
     }
 
@@ -34,12 +41,41 @@ public class XRClickable : MonoBehaviour
 
     public void InvokeClick()
     {
+        if (_pressRoutine != null)
+            StopCoroutine(_pressRoutine);
+
+        _pressRoutine = StartCoroutine(PressFeedback());
         _onClick?.Invoke();
+    }
+
+    private IEnumerator PressFeedback()
+    {
+        _pressed = true;
+        transform.localScale = _normalScale * 0.97f;
+        ApplyColor();
+
+        yield return new WaitForSecondsRealtime(0.09f);
+
+        _pressed = false;
+        transform.localScale = _normalScale;
+        ApplyColor();
+        _pressRoutine = null;
+    }
+
+    private void OnDisable()
+    {
+        transform.localScale = _normalScale;
+        _pressed = false;
     }
 
     private void ApplyColor()
     {
-        if (_targetImage != null)
+        if (_targetImage == null)
+            return;
+
+        if (_pressed)
+            _targetImage.color = _pressedColor;
+        else
             _targetImage.color = _hovered ? _hoverColor : _normalColor;
     }
 }
